78e2538c45dd118bed256c0b51ff182d88edf85b
Cozmu <www.jorgereis@gmail.com>
2023-07-21 20:25:09 -0300

    feat: implementando função scrape_news (req 4)

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,13 +1,15 @@
 import requests
 import time
 from parsel import Selector
+# from tech_news.database import create_news
 
 
 def fetch(url):
     try:
         time.sleep(1)
-        result = requests.get(url, headers={
-            "user-agent": "Fake user-agent"}, timeout=3)
+        result = requests.get(
+            url, headers={"user-agent": "Fake user-agent"}, timeout=3
+        )
         if result.status_code == 200:
             return result.text
         else:
@@ -18,23 +20,38 @@ def fetch(url):
 
 def scrape_updates(html_content):
     selector = Selector(text=html_content)
-    list_links = selector.css('.cs-overlay a::attr(href)').getall()
+    list_links = selector.css(".cs-overlay a::attr(href)").getall()
     return list_links
 
 
 def scrape_next_page_link(html_content):
     selector = Selector(text=html_content)
-    next_link = selector.css('.nav-links .next::attr(href)').get()
+    next_link = selector.css(".nav-links .next::attr(href)").get()
     return next_link
 
 
-# Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    selector = Selector(text=html_content)
+    url = selector.css('head link[rel="canonical"]::attr(href)').get()
+    title = selector.css(".entry-header-inner h1::text").get().strip()
+    timestamp = selector.css(".post-meta .meta-date::text").get()
+    writer = selector.css("span.author a::text").get()
+    reading_time = selector.css(".post-meta .meta-reading-time::text").get()
+    sumary = "".join(
+        selector.css(".entry-content > p:first-of-type *::text").getall()
+    ).strip()
+    category = selector.css(".category-style .label::text").get()
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": int(reading_time.split(" ")[0]),
+        "summary": sumary,
+        "category": category,
+    }
 
 
-# Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
     raise NotImplementedError
